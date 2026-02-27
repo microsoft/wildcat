860791aea66b703bf4b54b068ccc5b0e9f936265
tobias-schroeder <t.schroeder21@imperial.ac.uk>
2026-02-27 13:08:47 +0000

    default dat_per_class is 5

--- examples/biggan/demo_generate_images.py
+++ examples/biggan/demo_generate_images.py
@@ -35,7 +35,7 @@ def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--model_name",type=str, default='biggan-deep-512')
     parser.add_argument("--num_classes",type=int, default=1000)
-    parser.add_argument("--data_per_class",type=int, default=1)
+    parser.add_argument("--data_per_class",type=int, default=5)
     parser.add_argument("--seed",type=int, default=1)
     parser.add_argument("--num_splits", "-ns",type=int, default=10)
     parser.add_argument("--batch_size",type=int, default=32)
