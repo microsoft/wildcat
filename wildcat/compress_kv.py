93e0a28d4371f0b73d65b5b9197386b6612da98c
Lester Mackey <lmackey@microsoft.com>
2026-08-06 15:56:36 +0000

    Adding optimal temperature parameter

--- wildcat/compress_kv.py
+++ wildcat/compress_kv.py
@@ -117,11 +117,7 @@ def compress(
 
     return compress_kv(keys, values, r, scale)
 
-
-
-# Two times the constant rho_0 = sqrt(1+exp(2W_0(2/e^2)+2))
-# up to machine precision
-TWO_RHO_0 = 6.383202050647408
+RHO_2 = 3.94582
 def find_kernel_temperature(
         scale,
         q_scale,
@@ -140,11 +136,6 @@ def find_kernel_temperature(
     Returns: tau (torch.Tensor): shape (batch_dims, 1)
     """
 
-    if phi is not None:
-        N = N*phi**2
-
-    b = math.log(N)/(scale*q_scale*k_scale) + 2.
-    upper = b/(2*lambert_w_circ_exp((b/TWO_RHO_0).log()))
-    tau = torch.sqrt(k_scale/q_scale * upper)
+    tau = torch.sqrt(k_scale/q_scale * RHO_2)
 
     return tau
